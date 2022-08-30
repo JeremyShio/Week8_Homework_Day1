29ea5924a3395846e03e4b7e9ddd33a3b6e1f666
Jeremy <jeremy.shiotani@gmail.com>
2022-08-30 07:25:07 -0700

    Oops, was not in app folder when committing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+import Navbar from './components/Navbar';
+// import Button from './components/Button';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+
+
+function App(props) {
+
+    // Set a state for count - inital state of 0 and setCound is function to change state value of count
+    const [count, setCount] = useState(0)
+    // Set a state for strings - initial state of [] and setNames is function to change state value of strings
+    const [strings, setStrings] = useState([])
+
+
+    // Function which executes when the main form is submitted
+    function handleStringPress(i) {
+        // Prevent the default page-refresh from occuring
+        i.preventDefault();
+        // Get the value from the user input form
+        const string = i.target.stringName.value;
+        // Set the state of strings to be the current state of strings + the new string
+        let newStrings = [...strings, string];
+        setStrings(newStrings);
+    };
+
+
+    // Function which executes when a string is added to list
+    function handlePress(step) {
+        console.log('Added to your To-Do-List!');
+        // Add 1 to list total for each string added
+        step = 1
+        // Set the state of count to be the current state of count + step
+        setCount(count + step);
+    };
+
+
+    return (
+        // JSX expressions
+        <>
+            {/* Call Navbar component */}
+            <Navbar brand = 'YOUR-LIST' />
+            <div className = 'container'>
+                <h1 className = 'text-center'>To-Do-List!</h1>
+                <h3 className = 'text-center'>Total: {count}</h3>
+                <form onSubmit = {handleStringPress} handlePress = {handlePress}>
+                    <input type = 'text' className = 'form-control' name = 'stringName' />
+                    <input type = 'submit' value = 'Submit' />
+                </form>
+                {strings.map((s, index) => <p key = {index}>{s}</p>)}
+            </div>
+        </>
+    );
+
+};
+
+
+
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Navbar(props) {
+    return (
+        <nav className = "navbar navbar-expand-lg navbar-light bg-light">
+            <div className = "container-fluid">
+                {/* JSX expression used to call props.name props.city props.a */}
+                <a className = "navbar-brand" href = "/">{props.brand}</a>
+                <button className = "navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span className = "navbar-toggler-icon"></span>
+                </button>
+                <div className = "collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className = "navbar-nav">
+                    <a className = "nav-link active" aria-current="page" href = "/">Home</a>
+                    <a className = "nav-link" href = "/">My-List</a>
+                    <a className = "nav-link" href = "/">About</a>
+                </div>
+                </div>
+            </div>
+        </nav>
+    )
+};
